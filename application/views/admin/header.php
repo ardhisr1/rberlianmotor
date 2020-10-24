0a9ea50da14850aef308347d7525584ae16c11c6
unknown <radika.kerenz1@gmail.com>
2020-10-24 15:54:45 +0700

    fix route

--- application/views/admin/header.php
+++ application/views/admin/header.php
@@ -41,10 +41,10 @@
         <a class="nav-link" data-widget="pushmenu" href="#" role="button"><i class="fas fa-bars"></i></a>
       </li>
       <li class="nav-item d-none d-sm-inline-block">
-        <a href="index3.html" class="nav-link">Home</a>
+        <a href="<?= base_url() ?>admin-dashboard" class="nav-link">Home</a>
       </li>
       <li class="nav-item d-none d-sm-inline-block">
-        <a href="#" class="nav-link">Transaksi</a>
+        <a href="<?= base_url() ?>admin-dashboard" class="nav-link">Transaksi</a> <!-- sementara -->
       </li>
     </ul>
 
