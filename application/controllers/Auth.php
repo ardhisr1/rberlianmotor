1eadec936db8db35d54a4b7481bd4b9704d1e6ea
prioarief <prioariefgunawangunawan@gmail.com>
2020-10-25 13:08:14 +0700

    Authentication Admin

--- application/controllers/Auth.php
+++ application/controllers/Auth.php
@@ -9,13 +9,18 @@ class Auth extends CI_Controller
         $this->load->model('AuthModel', 'Model');
     }
 
-    function validate($login = false)
+    function validate($login = false, $isAdmin = false)
     {
         $validation = $this->form_validation;
         if (!$login) {
             $validation->set_rules('name', 'name', 'required');
-            $validation->set_rules('email', 'email', 'required|valid_emails|is_unique[customers.email]');
             $validation->set_rules('password', 'password', 'required|min_length[8]');
+
+            if(!$isAdmin){
+                $validation->set_rules('email', 'email', 'required|valid_emails|is_unique[customers.email]');
+            }else{
+                $validation->set_rules('email', 'email', 'required|valid_emails|is_unique[admin.email]');
+            }
         } else {
             $validation->set_rules('email', 'email', 'required|valid_emails');
             $validation->set_rules('password', 'password', 'required');
@@ -75,6 +80,42 @@ class Auth extends CI_Controller
             $data = $this->setData();
             $this->Model->registerUser($data);
             redirect('auth/login');
+        }
+    }
+
+    public function loginAdmin()
+    {
+        $validation = $this->validate(true);
+        if (!$validation) {
+            $this->load->view('auth-admin/login');
+        } else {
+            $data = $this->setData(true);
+            $emailCheck = $this->Model->getAdminByEmail($data['email']);
+            if ($emailCheck) {
+                $passwordHash = $emailCheck['password'];
+                if(password_verify($data['password'], $passwordHash)){
+                    echo 'login success';
+                }else{
+                    // Password wrong
+                    echo 'password wrong';
+                    // redirect('auth/login');
+                }
+            }else{
+                // Email is not registered
+                redirect('auth/login');
+            }
+        }
+    }
+
+    public function addAdmin()
+    {
+        $validation = $this->validate(false, true);
+        if (!$validation) {
+            $this->load->view('auth-admin/register');
+        } else {
+            $data = $this->setData();
+            $this->Model->registerAdmin($data);
+            redirect('auth/login'); // Nanti redirect ke manage admin aja 
         }
     }
 }
--- application/models/AuthModel.php
+++ application/models/AuthModel.php
@@ -1,13 +1,25 @@
-<?php 
+<?php
 
-class AuthModel extends CI_Model{
+class AuthModel extends CI_Model
+{
     public function registerUser($data)
     {
         return $this->db->insert('customers', $data);
     }
 
+
     public function getUserByEmail($email)
     {
         return $this->db->get_where('customers', ['email' => $email])->row_array();
+    }
+
+    public function registerAdmin($data)
+    {
+        return $this->db->insert('admin', $data);
+    }
+
+    public function getAdminByEmail($email)
+    {
+        return $this->db->get_where('admin', ['email' => $email])->row_array();
     }
 }
